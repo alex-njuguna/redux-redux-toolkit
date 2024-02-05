e501d17512719a96e3793a45449ee6775bba8754
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-06 00:01:51 +0300

    root reducer

---
+++ redux1/src/reducers/index.js
@@ -0,0 +1,9 @@
+// root reducer
+import taskReducer from "./taskReducer";
+import { combineReducers } from "redux";
+
+const rootReducer = combineReducers({
+  tasks: taskReducer,
+});
+
+export default rootReducer;
