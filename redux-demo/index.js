4f84a8b072435bcd3befcc1b783ab44919191bbb
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-05 15:46:45 +0300

    first reducer

--- redux-demo/index.js
+++ redux-demo/index.js
@@ -1,8 +1,22 @@
-const CAKE_ORDERED = 'CAKE_ORDERED'
+const CAKE_ORDERED = "CAKE_ORDERED";
 
-function orderCake(){
-   return {
-        type: CAKE_ORDERED,
-        quantity: 1
-    }
+function orderCake() {
+  return {
+    type: CAKE_ORDERED,
+    quantity: 1,
+  };
 }
+
+const initialState = {
+  numOfCakes: 10,
+};
+
+// (previousState, action) => newState
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case CAKE_ORDERED:
+      return { numOfCakes: state.numOfCakes - 1 };
+    default:
+      return state;
+  }
+};
