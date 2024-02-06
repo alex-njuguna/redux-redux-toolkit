6d634cb353f1ab182cbb03d62d9ae867bc66b13d
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-06 22:39:15 +0300

    tailwind changes and store configuration

--- redux-store/src/App.js
+++ redux-store/src/App.js
@@ -1,11 +1,13 @@
 import "./App.css";
 import Main from "./components/main/Main";
 import Navbar from "./components/navbar/Navbar";
+import Slider from "./components/slider/Slider";
 
 export default function App() {
   return (
     <div className="App">
       <Navbar />
+      <Slider/>
     </div>
   );
 }
--- redux-store/src/app/store.js
+++ redux-store/src/app/store.js
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-
+import sliderReducer from '../features/slices/sliderSlice'
 
 export const store = configureStore({
   reducer: {
-    
+    slider: sliderReducer,
   },
 });
