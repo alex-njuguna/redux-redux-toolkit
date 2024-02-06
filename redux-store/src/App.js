a9858b1ac3154efa209755484b94db82e45e92b2
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-06 15:21:06 +0300

    tailwind and font family

--- redux-store/src/App.js
+++ redux-store/src/App.js
@@ -1,5 +1,9 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Hello everyone</div>;
+  return (
+    <div className="App">
+      <h1 class="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
