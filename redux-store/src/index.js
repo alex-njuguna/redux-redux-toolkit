5e80bc4fb665f929baf33d0c2b8d8565d97dbddf
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-07 01:27:44 +0300

    navigate buttons

--- redux-store/src/index.js
+++ redux-store/src/index.js
@@ -1,19 +1,20 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import { store } from './app/store';
-import App from './App';
-import './index.css';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
+import { store } from "./app/store";
+import App from "./App";
+import "./index.css";
+import { ThemeProvider } from "@material-tailwind/react";
 
-const container = document.getElementById('root');
+const container = document.getElementById("root");
 const root = createRoot(container);
 
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <ThemeProvider>
+        <App />
+      </ThemeProvider>
     </Provider>
   </React.StrictMode>
 );
-
-
