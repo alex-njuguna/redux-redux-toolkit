9a4d26e20d985fbcb1fe657558c192494283fde5
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-07 03:02:11 +0300

    routes definition

--- redux-store/src/App.js
+++ redux-store/src/App.js
@@ -1,10 +1,21 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 import "./App.css";
 import Main from "./components/main/Main";
+import FilteredProducts from "./components/filteredProducts/FilteredProducts";
 
 export default function App() {
   return (
     <div className="App">
-      <Main />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route
+            path="/filtered-products/:type"
+            element={<FilteredProducts />}
+          />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
---
+++ redux-store/src/components/filteredProducts/FilteredProducts.jsx
@@ -0,0 +1,3 @@
+export default function FilteredProducts() {
+  return <div>filtered products</div>;
+}
