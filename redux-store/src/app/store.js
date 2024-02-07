59b2a188e4a9412fc00245146d6eaef4a82335ac
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-07 03:26:02 +0300

    productsSlice and store configuration

--- redux-store/src/app/store.js
+++ redux-store/src/app/store.js
@@ -1,8 +1,10 @@
-import { configureStore } from '@reduxjs/toolkit';
-import sliderReducer from '../features/slices/sliderSlice'
+import { configureStore } from "@reduxjs/toolkit";
+import sliderReducer from "../features/slices/SliderSlice";
+import productsReducer from "../features/slices/ProductsSlice";
 
 export const store = configureStore({
   reducer: {
     slider: sliderReducer,
+    products: productsReducer,
   },
 });
