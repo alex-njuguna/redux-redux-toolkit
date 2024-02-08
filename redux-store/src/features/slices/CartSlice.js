75b889fcbef14c8a9d84b71a2876eae70388b3ef
alex-njuguna <njugunakinuthia013@gmail.com>
2024-02-08 04:23:07 +0300

    remove from cart slice

--- redux-store/src/features/slices/CartSlice.js
+++ redux-store/src/features/slices/CartSlice.js
@@ -37,8 +37,36 @@ export const cartSlice = createSlice({
         console.log(error);
       }
     },
+    removeFromCart(state, action) {
+      const product = action.payload;
+
+      try {
+        const existingProductIndex = state.cart.findIndex(
+          (item) =>
+            item.id === product.id &&
+            item.size === product.size &&
+            item.color === product.color
+        );
+
+        if (existingProductIndex >= 0) {
+          const existingProduct = state.cart[existingProductIndex];
+          if (existingProduct.amount === 1) {
+            state.cart.splice(existingProductIndex, 1);
+            state.totalAmount--;
+            state.totalPrice -= existingProduct.totalPrice;
+          } else {
+            existingProduct.amount--;
+            existingProduct.totalPrice -= product.price;
+            state.totalAmount--;
+            state.totalPrice -= product.price;
+          }
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
   },
 });
 
-export const { addToCart } = cartSlice.actions;
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
